1f6c456db6f84bd4042d58143de2b830c9fa9867
Hendrik Heneke <hendrik@pixelwunder.net>
2016-03-18 11:29:51 +0100

    PersistenceUnitInfo.

---
+++ src/KaderORMPersistenceUnitInfo.php
@@ -0,0 +1,14 @@
+<?php
+namespace Kader\ORM;
+
+class KaderORMPersistenceUnitInfo
+{
+
+    /**
+     * @return array
+     */
+    public static function getSourceDirectories()
+    {
+        return [__DIR__];
+    }
+}
